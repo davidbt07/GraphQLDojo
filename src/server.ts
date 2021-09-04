0e22bc0a5c08b3027de8fca420bc5fb75e566ede
davidbt07 <david.ballesteros@udea.edu.co>
2021-09-04 11:28:53 -0500

    first commit

---
+++ src/server.ts
@@ -0,0 +1,34 @@
+import express from 'express'
+import compression from 'compression'
+import cors from 'cors'
+import { createServer } from 'http'
+import schema from './schema'
+import {ApolloServer} from 'apollo-server-express'
+import expressPlayGround from 'graphql-playground-middleware-express'
+
+const app = express()
+app.use(cors())
+app.use(compression())
+
+const servidor = new ApolloServer({
+    schema,
+    introspection: true
+})
+
+servidor.applyMiddleware({app});
+
+
+
+app.get('/', expressPlayGround({
+    endpoint: '/graphql'
+}));
+
+
+
+const puerto = 5400
+const httpServer = createServer(app)
+
+httpServer.listen(
+    {port: puerto},
+    () => console.log('Escuchando en puerto: http://localhost:'+puerto+'/graphql')
+)
